ffad4854eceb2df2a27cf79a2c3178b880c1c833
SeanKuehl <teddscottfelt@gmail.com>
2023-01-16 09:18:19 -0500

    added doxygen comments to winsock and allegro helper libs

--- HelperLibraries/Allegro/AllegroHelper.cpp
+++ HelperLibraries/Allegro/AllegroHelper.cpp
@@ -1,5 +1,9 @@
 #include "AllegroHelper.h"
 
+/*! Start allegro lib resources.
+*   Return -1 if there is an error initializing
+*	or 1 if everything worked.
+*/
 int InitializeAllegro() {
     if (!al_init())
     {
@@ -9,6 +13,10 @@ int InitializeAllegro() {
     return SUCCESS;
 }
 
+/*! Start allegro image addon resources.
+*   Return -1 if there is an error initializing
+*	or 1 if everything worked.
+*/
 int InitializeImageAddon() {
     if (!al_init_image_addon())
     {
@@ -18,6 +26,10 @@ int InitializeImageAddon() {
     return SUCCESS;
 }
 
+/*! install allegro keyboard resources.
+*   Return -1 if there is an error initializing
+*	or 1 if everything worked.
+*/
 int InstallKeyboard() {
     if (!al_install_keyboard())
     {
@@ -27,6 +39,12 @@ int InstallKeyboard() {
     return SUCCESS;
 }
 
+/*! Create an allegro timer object
+*   Return NULL if there is an error initializing
+*	or a new ALLEGRO_TIMER* object if everything worked.
+*   \param firstTime this is the first part of the timer fps equation(currently 1.0)
+*   \param secondTime this is the second part of the timer fps equation(currently 30.0)
+*/
 ALLEGRO_TIMER* CreateTimer(double firstTime, double secondTime) {
     ALLEGRO_TIMER* timer = al_create_timer(firstTime / secondTime); //default 1.0 / 30.0
     if (!timer)
@@ -38,6 +56,10 @@ ALLEGRO_TIMER* CreateTimer(double firstTime, double secondTime) {
     return timer;
 }
 
+/*! Create an allegro event queue object
+*   Return NULL if there is an error initializing
+*	or a new ALLEGRO_EVENT_QUEUE* object if everything worked.
+*/
 ALLEGRO_EVENT_QUEUE* CreateQueue() {
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
     if (!queue)
@@ -48,6 +70,12 @@ ALLEGRO_EVENT_QUEUE* CreateQueue() {
     return queue;
 }
 
+/*! Create an allegro display object
+*   Return NULL if there is an error initializing
+*	or a new ALLEGRO_DISPLAY* object if everything worked.
+*   \param screenWidth width of new screen to create
+*   \param screenHieght hieght of new screen to create
+*/
 ALLEGRO_DISPLAY* CreateDiplay(int screenWidth, int screenHieght) {
     ALLEGRO_DISPLAY* disp = al_create_display(screenWidth, screenHieght);
     if (!disp)
@@ -58,6 +86,10 @@ ALLEGRO_DISPLAY* CreateDiplay(int screenWidth, int screenHieght) {
     return disp;
 }
 
+/*! Create an allegro font object
+*   Return NULL if there is an error initializing
+*	or a new ALLEGRO_FONT* object if everything worked.
+*/
 ALLEGRO_FONT* CreateBuiltInFont() {
     ALLEGRO_FONT* font = al_create_builtin_font();
     if (!font)
@@ -68,6 +100,11 @@ ALLEGRO_FONT* CreateBuiltInFont() {
     return font;
 }
 
+/*! Create an allegro image object
+*   Return NULL if there is an error initializing
+*	or a new ALLEGRO_BITMAP* object if everything worked.
+*   \param path this is the path to the image to load
+*/
 ALLEGRO_BITMAP* CreateImage(std::string path) {
     ALLEGRO_BITMAP* image = al_load_bitmap(path.c_str());
     if (!image)
@@ -78,6 +115,11 @@ ALLEGRO_BITMAP* CreateImage(std::string path) {
     return image;
 }
 
+/*! Register various allegro event sources
+*   \param queue the event queue to register
+*   \param display the display to register
+*   \param timer the timer to register
+*/
 void RegisterEventSources(ALLEGRO_EVENT_QUEUE* queue, ALLEGRO_DISPLAY* display, ALLEGRO_TIMER* timer) {
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(display));
--- HelperLibraries/Networking/Winsock.cpp
+++ HelperLibraries/Networking/Winsock.cpp
@@ -1,6 +1,9 @@
 #include "Winsock.h"
 
-
+/*! Start needed winsock resources.
+*   Return -1 if there is an error initializing
+*	or 1 if everything worked.
+*/
 int InitializeWinsock() {
 
 	WSADATA wsa;
@@ -15,6 +18,8 @@ int InitializeWinsock() {
 	return SUCCESS;
 }
 
+/*! Try to createa a socket. Print error if failed.
+*/
 SOCKET CreateSocket() {
 
 	SOCKET s;
@@ -27,6 +32,12 @@ SOCKET CreateSocket() {
 	return s;
 }
 
+/*! Given a client socket, ip and port,
+*   try to connect to the server.
+*   \param s this is the passed client socket
+*   \param ip this is the ip address like "127.0.0.1"
+*   \param port this is something like 8080 
+*/
 int ConnectToServer(SOCKET s, string ip, int port) {
 
 	struct sockaddr_in server;
@@ -47,6 +58,14 @@ int ConnectToServer(SOCKET s, string ip, int port) {
 
 }
 
+/*! Given a socket and a string message,
+*   send the message over the network via
+*   255 byte sized buffer with ';' as end
+*   character added on for control.
+*   return -1 if failed and 1 is worked
+*   \param s passed SOCKET object
+*   \param message a string message to be sent
+*/
 int SendString(SOCKET s, string message) {
 	//last char in buffer from message will always be ';' to show where
 	//message string ends
@@ -75,7 +94,12 @@ int SendString(SOCKET s, string message) {
 
 
 
-
+/*! Given a server socket and port,
+*   try to bind it to that port.
+*   return -1 if failed and 1 if worked
+*   \param s this is the passed server socket
+*   \param port this is the port at the ip that the server will try to bind to
+*/
 int BindServer(SOCKET s, int port) {
 	struct sockaddr_in server;
 	server.sin_family = AF_INET;
@@ -91,6 +115,11 @@ int BindServer(SOCKET s, int port) {
 	return SUCCESS;
 }
 
+/*! Given a server socket, accept connections
+*   to this server. Return the connected socket and
+*   if failed print out an error message.
+*   \param s this is the server socket
+*/
 SOCKET AcceptConnection(SOCKET s) {
 	SOCKET new_socket;
 	struct sockaddr_in client;
@@ -106,6 +135,13 @@ SOCKET AcceptConnection(SOCKET s) {
 
 }
 
+/*! Given a socket, try to receive a message.
+*   this message will be a string and the buffer
+*   will have a ';' as the last character.
+*   Return the message if successful and 
+*   "ERROR" if not
+*   \param new_socket this is the socket to receive the message
+*/
 string ReceiveString(SOCKET new_socket) {
 
 	char server_reply[BUFFER_SIZE];
